7437a3341ddd4772de68bad760f62f79995b1f9d
Sandrine Orsini <atelier@ld-sine.fr>
2025-12-03 12:19:12 +0000

    File changes

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -152,14 +152,16 @@ export default function Welcome() {
           <p className="text-gray-400 mb-8">
             Rejoins des centaines d'entrepreneurs qui ont déjà lancé leur activité grâce à PASSION IA.
           </p>
-          <GlowButton 
-            onClick={handleStart}
-            size="lg"
-            className="text-lg px-10"
-          >
-            <Sparkles className="w-5 h-5 mr-2" />
-            Commencer maintenant
-          </GlowButton>
+          <div className="flex justify-center">
+            <GlowButton 
+              onClick={handleStart}
+              size="lg"
+              className="text-lg px-10"
+            >
+              <Sparkles className="w-5 h-5 mr-2" />
+              Commencer maintenant
+            </GlowButton>
+          </div>
         </div>
       </motion.div>
     </div>

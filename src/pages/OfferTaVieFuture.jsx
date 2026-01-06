6a942b8f373356396b3a85bf615aa68aae61c9c0
Alfred Orsini <atelier@ld-sine.fr>
2026-01-06 20:15:23 +0000

    File changes

--- src/pages/OfferTaVieFuture.jsx
+++ src/pages/OfferTaVieFuture.jsx
@@ -414,6 +414,7 @@ export default function OfferTaVieFuture() {
             </GlowButton>
           </motion.div>
         </div>
+        </div>
       </div>
 
       {/* Transition Animation */}

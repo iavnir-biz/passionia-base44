f10d55f034426bb8032edbf5820efb5c33600b56
IAVNIR <contact@iavnir.fr>
2026-03-05 13:37:39 +0100

    Delete src/pages/MonetiserSavoir.jsx

--- src/pages/MonetiserSavoir.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Navigate } from 'react-router-dom';
-import { createPageUrl } from '@/utils';
-
-// Cette page a été supprimée - redirection vers Welcome
-export default function MonetiserSavoir() {
-  return <Navigate to={createPageUrl('Welcome')} replace />;
-}
